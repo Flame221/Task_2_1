b7d51f5d8ebd871e49682d2d792fde4dfb4db223
Dima <thelordser221@gmail.com>
2018-11-22 15:55:24 +0200

    Initial commit

---
+++ src/main/java/Character.java
@@ -0,0 +1,39 @@
+public class Character {
+    private String name;
+    private int damage;
+    private int health;
+
+    public Character(String name, int damage, int health) {
+        this.name = name;
+        this.damage = damage;
+        this.health = health;
+    }
+
+    public Character() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        Character hero = new Character("Hero", 2, 11);
+        Character monster = new Character("Monster", 4, 8);
+
+        String string = BattleStage.startBattle(hero, monster);
+        System.out.println(string);
+    }
+
+}
+
+class BattleStage {
+    private BattleStage() {
+    }
+
+    public static String startBattle(Character hero, Character monster) {
+        String string;
+        String monsterName = monster.getName();
+        String heroName = hero.getName();
+
+        int heroDamage = hero.getDamage();
+        int heroHealth = hero.getHealth();
+
+        int monsterDamage = monster.getDamage();
+        int monsterHealth = monster.getHealth();
+
+        while (monsterHealth > 0 && heroHealth > 0) {
+            monsterHealth -= heroDamage;
+            System.out.println(heroName + " attacked the " + monsterName + ". " + monsterName + " has " + monsterHealth + " HP");
+            heroHealth -= monsterDamage;
+            System.out.println(monsterName + " attacked the " + heroName + ". " + heroName + " has " + heroHealth + " HP");
+        }
+
+        if (monsterHealth <= 0){
+            string = heroName + " wins!";
+        }
+        else if (heroHealth <= 0){
+            string = heroName + " wins!";
+        }
+        else {string = "ERROR!";}
+
+      return string;
+    }
+}
